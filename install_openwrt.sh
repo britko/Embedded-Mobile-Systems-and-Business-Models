f5b8d16cc4fc099ad3bbe2c46e2b33f5a15a8fdc
britko <britko@naver.com>
2021-11-05 03:10:45 +0900

    Update install_openwrt.sh

--- install_openwrt.sh
+++ install_openwrt.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+echo "Start install OpenWrt"
+
 # Build system setup (Debian / Ubuntu)
 # openwrt.org > Documentation > Developer guide > Build system setup
 sudo apt update -y && sudo apt upgrade -y
 
+# Requirements
 sudo apt install build-essential ccache ecj fastjar file g++ gawk \
 gettext git java-propose-classpath libelf-dev libncurses5-dev \
 libncursesw5-dev libssl-dev python python2.7-dev python3 unzip wget \
 python3-distutils python3-setuptools python3-dev rsync subversion \
-swig time xsltproc zlib1g-dev
+swig time xsltproc zlib1g-dev binutils bzip2 flex gawk gcc-6+ grep \
+libc-dev libz-dev perl rsync subversion unzip
 
 # Clone OpenWrt GitHub (https://github.com/openwrt/openwrt)
 git clone https://github.com/openwrt/openwrt.git
 cd openwrt
 
-# Requirements
-sudo apt install binutils bzip2 diff find flex gawk gcc-6+ getopt grep install libc-dev libz-dev \
-make4.1+ perl python3.6+ rsync subversion unzip which
-
 # Setup branch (https://openwrt.org/docs/guide-developer/toolchain/use-buildsystem)
 git checkout openwrt-21.02
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+echo "------------------------------------------------"
+echo "OpenWrt installation completed!"
